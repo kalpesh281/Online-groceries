9dc9d110c38b2b4044f41464368644691b87eb98
Kalpesh Senva <shenvakalpesh4@gmail.com>
2023-02-11 21:33:50 +0530

    Improve all file

--- Js/script.js
+++ Js/script.js
@@ -2,6 +2,9 @@ let searchForm=document.querySelector('.form-search');
 
 document.querySelector('#search-btn').onclick =()=>{
     searchForm.classList.toggle('active')
+    navbar.classList.remove('active')
+    loginCart.classList.remove('active')
+    shoppingCart.classList.remove('active')
 }
 
 
@@ -9,4 +12,34 @@ let shoppingCart=document.querySelector('.shopping-cart');
 
 document.querySelector('#cart-btn').onclick =()=>{
     shoppingCart.classList.toggle('active')
+    searchForm.classList.remove('active')
+    navbar.classList.remove('active')
+    loginCart.classList.remove('active')
+    
+}
+
+
+let loginCart=document.querySelector('.login-form');
+
+document.querySelector('#login-btn').onclick =()=>{
+    loginCart.classList.toggle('active')
+    searchForm.classList.remove('active')
+    navbar.classList.remove('active')
+    shoppingCart.classList.remove('active')
+}
+
+let navbar=document.querySelector('navbar');
+
+document.querySelector('#menu-btn').onclick =()=>{
+    navbar.classList.toggle('active')
+    searchForm.classList.remove('active')
+    loginCart.classList.remove('active')
+    shoppingCart.classList.remove('active')
+}
+
+window.onscroll =()=>{
+    searchForm.classList.remove('active')
+    navbar.classList.remove('active')
+    loginCart.classList.remove('active')
+    shoppingCart.classList.remove('active')
 }
