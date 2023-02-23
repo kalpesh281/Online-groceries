5cfc4ada80924f1eb24000e8b749983feaf04229
Kalpesh Senva <shenvakalpesh4@gmail.com>
2023-02-23 23:33:04 +0530

    Add all file

--- Js/script.js
+++ Js/script.js
@@ -39,7 +39,32 @@ document.querySelector('#menu-btn').onclick =()=>{
 
 window.onscroll =()=>{
     searchForm.classList.remove('active')
-    navbar.classList.remove('active')
+   
     loginCart.classList.remove('active')
     shoppingCart.classList.remove('active')
 }
+
+
+var swiper = new Swiper(".product-slider", {
+    loop: true, 
+    spaceBetween:20,
+   autoplay:{
+    delay:7500,
+    disableOnInteraction:false
+   },
+   centeredSlides:true,
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+        
+      },
+      768: {
+        slidesPerView: 2,
+        
+      },
+      1020: {
+        slidesPerView: 3,
+        
+      },
+    },
+  });
